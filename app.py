842e8665c45be74bf22c56e26c0600bc51ba75d1
sathyaprakashmAIML <sathyaprakashmsd@gmail.com>
2025-02-22 11:41:52 +0530

    commited

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from langchain_community.document_loaders import YoutubeLoader
 from langchain_core.prompts import PromptTemplate
 from langchain_core.prompts import ChatPromptTemplate
-import edge_tts
 from langchain_core.output_parsers import StrOutputParser
 import os
 from dotenv import load_dotenv
